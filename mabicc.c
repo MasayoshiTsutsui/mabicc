7681a3ac62f78e9a7ddc9c74f687d72de68135eb
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-06-26 17:07:36 +0900

    New Impl: muldiv compilable & AST parsing

--- mabicc.c
+++ mabicc.c
@@ -5,6 +5,13 @@
 #include <stdarg.h>
 #include <string.h>
 
+#define DEBUG 0
+
+void dbgprint(char *fmt, ...) {
+    if (DEBUG)
+        printf("%s", fmt);
+}
+
 
 // tokens
 typedef enum {
@@ -32,6 +39,13 @@ char *user_input;
 
 // func for reporting errors
 // same ary as printf
+void error(char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    vfprintf(stderr, fmt, ap);
+    fprintf(stderr, "\n");
+    exit(1);
+}
 
 void error_at(char *loc, char *fmt, ...) {
     va_list ap;
@@ -87,7 +101,8 @@ Token *new_token(TokenKind kind, Token *cur, char *str) {
 }
 
 // tokenize input char 'p' and return it.
-Token *tokenize(char *p) {
+Token *tokenize() {
+    char *p = user_input;
     Token head;
     head.next = NULL;
     Token *cur = &head;
@@ -99,7 +114,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (strchr("+-*/()", *p)) {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -117,37 +132,133 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+
+// node kinds of ast
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // integer
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind; // type of node
+    Node *lhs;     // left subtree
+    Node *rhs;     // right subtree
+    int val;       // used when kind is ND_NUM
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+    
+    for (;;) {
+        if (consume('+'))
+            node = new_node(ND_ADD, node, mul());
+        else if (consume('-'))
+            node = new_node(ND_SUB, node, mul());
+        else
+            return node;
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+
+    for (;;) {
+        if (consume('*'))
+            node = new_node(ND_MUL, node, primary());
+        else if (consume('/'))
+            node = new_node(ND_DIV, node, primary());
+        else
+            return node;
+    }
+}
+
+Node *primary() {
+    // if next token is '(', there should be '(' expr ')'
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    // otherwise, number
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if (argc != 2) {
-        fprintf(stderr, "invalid number of inputs.\n");
+        error("invalid number of inputs.\n");
         return 1;
     }
 
+    // tokenize & parse
     user_input = argv[1];
+    token = tokenize();
+    Node *node = expr();
 
-    // tokenize
-    token = tokenize(argv[1]);
-
+    // prefix of assembly
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // beginning of formula must be a number.
-    // check it and output first mov instr.
-    printf("  mov rax, %d\n", expect_number());
+    // track ast and generate codes
+    gen(node);
 
-    // consume tokens like `+ <num>` or `- <num>` 
-    // and output assembly
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-    
+    // there'd be the final result on stack top
+    // load it on rax and return it
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }

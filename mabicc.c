65da582671a9bf88d93963752cf951c415534cf6
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-06-26 17:27:48 +0900

    New Impl: Unary operator comilable

--- mabicc.c
+++ mabicc.c
@@ -12,7 +12,6 @@ void dbgprint(char *fmt, ...) {
         printf("%s", fmt);
 }
 
-
 // tokens
 typedef enum {
     TK_RESERVED, // non-number
@@ -168,6 +167,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -184,16 +184,24 @@ Node *expr() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary() {
+    if (consume('+'))
+        return unary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), unary());
+    return primary();
 }
 
 Node *primary() {

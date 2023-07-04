3caa59802831ddc8c3a5e3b3ce4d78a6de3d4f93
MasayoshiTsutsui <tsutsui-masayoshi0528@g.ecc.u-tokyo.ac.jp>
2023-07-04 11:49:37 +0900

    Styling: isolate tokenizer from parser

--- parse.c
+++ parse.c
@@ -79,68 +79,6 @@ bool at_eof() {
     return token->kind == TK_EOF;
 }
 
-// make new token and chain it to 'cur'.
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-    Token *tok = calloc(1, sizeof(Token));
-    tok->kind = kind;
-    tok->str = str;
-    tok->len = len;
-    cur->next = tok;
-    return tok;
-}
-
-bool startswith(char *p, char *q) {
-    return memcmp(p, q, strlen(q)) == 0;
-}
-
-// tokenize input char 'p' and return it.
-Token *tokenize() {
-    char *p = user_input;
-    Token head;
-    head.next = NULL;
-    Token *cur = &head;
-
-    while (*p) {
-        // skip space
-        if (isspace(*p)) {
-            p++;
-            continue;
-        }
-
-        if (startswith(p, "==") || startswith(p, "!=") ||
-            startswith(p, "<=") || startswith(p, ">=")) {
-            cur = new_token(TK_RESERVED, cur, p, 2);
-            p += 2;
-            continue;
-        }
-
-        if ('a' <= *p && *p <= 'z') {
-            int strlen = strspn(p, "abcdefghijklmnopqrstuvwxyz");
-            cur = new_token(TK_IDENT, cur, p, strlen);
-            p += strlen;
-            continue;
-        }
-
-        if (strchr("+-*/()<>=;", *p)) {
-            cur = new_token(TK_RESERVED, cur, p++, 1);
-            continue;
-        }
-
-        if (isdigit(*p)) {
-            cur = new_token(TK_NUM, cur, p, 0);
-            char *q = p;
-            cur->val = strtol(p, &p, 10);
-            cur->len = p - q;
-            continue;
-        }
-
-        error_at(p, "Unable to tokenize.");
-    }
-
-    new_token(TK_EOF, cur, p, 0);
-    return head.next;
-}
-
 Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
     Node *node = calloc(1, sizeof(Node));
     node->kind = kind;
